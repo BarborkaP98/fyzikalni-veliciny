751aa5d413183e2c737676428afc2bdf0ba2ffd1
BarborkaP98 <barborkapittner@gmail.com>
2026-06-08 15:23:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-
+let veličiny = [
+  {
+    nazev: "rychlost",
+    znacka: "v",
+    jednotka: "m/s",
+    meridlo: "tachometr"
+  },
+  {
+    nazev: "hmotnost",
+    znacka: "m",
+    jednotka: "kg",
+    meridlo: "váhy"
+  },
+  {
+    nazev: "čas",
+    znacka: "t",
+    jednotka: "s",
+    meridlo: "stopky"
+  },
+  {
+    nazev: "teplota",
+    znacka: "T",
+    jednotka: "°C",
+    meridlo: "teploměr"
+  },
+  {
+    nazev: "síla",
+    znacka: "F",
+    jednotka: "N",
+    meridlo: "siloměr"
+  }
+];
